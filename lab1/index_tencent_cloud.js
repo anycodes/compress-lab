3af0e9d8868204836b9e348a0729f7e0a0614af6
anycodes <liuyu@xmail.tech>
2024-05-16 11:36:31 +0800

    update project

---
+++ lab1/index_tencent_cloud.js
@@ -0,0 +1,26 @@
+'use strict';
+/*
+To enable the initializer feature (https://help.aliyun.com/document_detail/156876.html)
+please implement the initializer function as below：
+exports.initializer = (context, callback) => {
+  console.log('initializing');
+  callback(null, '');
+};
+*/
+
+const _ = require('lodash');
+const axios = require('axios');
+const express = require('express');
+const dotenv = require('dotenv');
+const bcrypt = require('bcryptjs');
+const yaml = require('js-yaml');
+const qrcode = require('qrcode');
+const { encode } = require('blurhash');
+
+exports.main_handler = async (event, context) => {
+  console.log("Hello World")
+  console.log(event)
+  console.log(event["non-exist"])
+  console.log(context)
+  return event
+};
